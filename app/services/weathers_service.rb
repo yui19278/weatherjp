e6984e4fb6077567684a1967a39962e408c381a2
yui19278 <yui.tannitarinai@gmail.com>
2025-07-28 07:10:31 +0900

    weathers/indexまで通ったけどjsonのとこ上手く行ってなさそう

--- app/services/weathers_service.rb
+++ app/services/weathers_service.rb
@@ -1,24 +1,22 @@
-require "httparty"
-require "uri"
+require 'faraday'
 
-class WeatherService
-  include HTTParty
-  base_uri "https://api.openweathermap.org/data/2.5/weather"
-
-  def initialize(location)
-    @api_key = ENV["OPENWEATHER_API_KEY"]
-    @location = location
+class WeathersService
+  def call
+    # todo 後で下の内容をcallに移動
   end
 
-  def fetch_weather
-      options = {
-        query: {
-          q: "#{@location},jp",
-          appid: @api_key,
-          lang: "ja",
-          units: "metric" # 摂氏
-        }
-      }
-    seld.class.get("", options) # base_uriをどこまで切ればよいか分からない
+  def initialize()
+    @api_key = ENV["OPENWEATHER_API_KEY"]
+    @connection = Faraday.new(url:  "https://api.openweathermap.org")
+  end
+
+  # 現在の天気
+  def fetch_weather(location)
+    @connection.get("/data/2.5/weather") do |req|
+      req.params[:q] = "#{location},jp"
+      req.params[:appid] = @api_key
+      req.params[:lang] = "ja"
+      req.params[:units] = "metric" # 摂氏
+    end
   end
 end
