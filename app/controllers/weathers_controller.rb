0635adde2b7c80f48ac0736a077d41613faa7858
yui19278 <yui.tannitarinai@gmail.com>
2025-07-28 05:23:02 +0900

    仮実装　これからserviceに分離させる

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,12 +1,53 @@
+require "httparty"
+require "uri"
+
 class WeathersController < ApplicationController
-    # get /
-    def new
-        @history = history_list
-        @weather = Weather.new
+  def new
+  end
+
+  def show
+    @location = params[:location]
+    # WeatherServiceを呼び出して天気情報を取得
+    response = WeatherService.new(@location).fetch_weather
+    
+    if response.success?
+      weather_data = response.parsed_response
+      @weather = {
+        name: weather_data["name"],
+        description: weather_data["weather"].first["description"],
+        temp_max: weather_data["main"]["temp_max"],
+        temp_min: weather_data["main"]["temp_min"],
+        humidity: weather_data["main"]["humidity"],
+      }
+    else 
+      @error = "天気情報の取得に失敗しました。地域名を確認してください。"
     end
-    # post /weathers
-    def create
-        flash[:notice] = "天気を取得しました"
+  end
+
+  private
+  def validate_location
+    if params[:location].blank?
+      flash[:alert] = "入力が不正です。"
+      redirect_to root_path
     end
 end
 
+class WeatherService
+  include httparty
+  base_uri "https://api.openweathermap.org/data/2.5/weather"
+
+  def initialize(location)
+    api_key = ENV["OPENWEATHER_API_KEY"]
+    @options = {
+      query: {
+        q: "#{location},jp",
+        appid: api_key,
+        lang: "ja",
+      }
+    }
+  end
+
+  def fetch_weather
+    seld.class.get("", @options)
+  end
+end
