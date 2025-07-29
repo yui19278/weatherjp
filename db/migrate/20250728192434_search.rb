8cb4f3657feb499aaaa811fb53473f6cf84cb828
yui19278 <22nc051@ms.dendai.ac.jp>
2025-07-29 16:05:13 +0900

    raspi_backup

--- db/migrate/20250728192434_search.rb
+++ db/migrate/20250728192434_search.rb
@@ -1,6 +1,12 @@
 class Search < ActiveRecord::Migration[7.2]
   def change
-    drop_table :search_histories
-    drop_table :searh_histories
+    drop_table :search_histories, if_exists: true
+    create_table :search_histories, if_not_exists: true do |t|
+    t.string :user_token, null: false
+    t.string :location,   null: false
+    t.timestamps
+  end
+  add_index :search_histories, [:user_token, :location],
+            unique: true, if_not_exists: true
   end
 end
